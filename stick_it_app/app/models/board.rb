ef288eb6d716a5d3e04612959c54b1fbe6b9b6a0
fgc29 <fgc0077@gmail.com>
2014-08-03 15:42:39 -0400

    deleted duplicate file

--- stick_it_app/app/models/board.rb
+++
@@ -1,9 +0,0 @@
-class Board < ActiveRecord::Base
-  has_many :stickies
-  has_and_belongs_to_many :collaborators
-  has_many :users, through: :collaborators
-
-  validates :title, presence: true, on: :create
-  validates :title, uniqueness: true, on: :create
-
-end
