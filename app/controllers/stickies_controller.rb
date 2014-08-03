a29c34aab95b69e04f9d56820f70280c64da50af
fgc29 <fgc0077@gmail.com>
2014-08-02 21:50:30 -0400

    sticky model done

--- app/controllers/stickies_controller.rb
+++ app/controllers/stickies_controller.rb
@@ -1,3 +1,37 @@
 class StickiesController < ApplicationController
 
+  def index
+    @stickies = Sticky.all
+  end
+
+  def new
+    @sticky = Sticky.new
+  end
+
+  def create
+    @sticky = Sticky.new(sticky_params)
+  end
+
+  def show
+    @sticky = Sticky.find(params[:id])
+  end
+
+  def edit
+    @sticky = Sticky.find(params[:id])
+  end
+
+  def update
+    @sticky = Sticky.find(params[:id])
+  end
+
+  def destroy
+    @sticky = Sticky.find(params[:id])
+  end
+
+  private
+  def sticky_params
+    params.require(:sticky).permit(:title, :content)
+  end
+
+
 end
