73f499b6ac7d7f49531ceb42fdb2243004c997a2
fgc29 <fgc0077@gmail.com>
2014-08-03 15:37:49 -0400

    added new sicky feature

--- app/models/sticky.rb
+++ app/models/sticky.rb
@@ -2,7 +2,8 @@ class Sticky < ActiveRecord::Base
 
   has_and_belongs_to_many :categories
   has_one :board, through: :categories
-  belongs_to :users
+  belongs_to :users, through: :sticky_access_right
+
 
   validates :title, :content, presence: true
   validates :title, :content, uniqueness: true
