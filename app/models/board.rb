d819fe7bbd3e170dc7d3c99c7fa2796b4a11269d
charlyebrown <tran1226@yahoo.com>
2014-08-03 17:05:20 -0400

    updated board.rb

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,8 +1,9 @@
 class Board < ActiveRecord::Base
-  has_many :stickies
-  has_and_belongs_to_many :collaborators
-  has_many :users, through: :collaborators
+  has_many :stickies, through: categories
+  has_and_belongs_to_many :collaborations
+  has_many :users, through: :collaborations
 
   validates :title, presence: true, on: :create
   validates :title, uniqueness: true, on: :create
+  validates :categories, uniqueness: true
 end
