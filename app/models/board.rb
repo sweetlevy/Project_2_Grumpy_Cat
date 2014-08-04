8ea07f4f34dfc3c6007e36bf2b6713b5cf858327
fgc29 <fgc0077@gmail.com>
2014-08-04 18:24:48 -0400

    cleaned boards

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,8 +1,9 @@
 class Board < ActiveRecord::Base
   has_many :stickies, through: :categories, dependent: :destroy
   has_and_belongs_to_many :collaborations
-
+  #points back to user model, so that the user_id is added to creator_id
   belongs_to :creator, :foreign_key => "creator_id",class_name: "User"
+  #points back to user model, so that the user_id is added to editor_id
   belongs_to :editor, :foreign_key => "editor_id", class_name: "User"
   has_many :users, through: :collaborations
   has_many :categories
