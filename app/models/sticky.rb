d6a704014a9a6aa984f0501d5db37b15884addb6
fgc29 <fgc0077@gmail.com>
2014-08-04 19:34:37 -0400

    did sticky views

--- app/models/sticky.rb
+++ app/models/sticky.rb
@@ -1,10 +1,8 @@
 class Sticky < ActiveRecord::Base
   #points back to user model, so that the user_id is added to creator_id
-  belongs_to :creator, :foreign_key => "creator_id", :class_name
-  => "User"
+  belongs_to :creator, :foreign_key => "creator_id", class_name: "User"
   #points back to user model, so that the user_id is added to editor_id
-  belongs_to :editor, :foreign_key => "editor_id", :class_name
-  => "User"
+  belongs_to :editor, :foreign_key => "editor_id", class_name: "User"
   belongs_to :category
   has_one :board, through: :category
 
