01e4796773e96ed9186c29524805be6d7dcbd942
sweetlevy <saritlevy@hotmail.com>
2014-08-06 21:13:05 -0400

    rspec testing of board,sticky,user models and sticky,user features.  commented out the welcome_controller_spec file because it isn't currently being used.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,10 +7,14 @@ describe User do
   it { is_expected.to validate_presence_of :name }
   it { is_expected.to validate_presence_of :email }
   it { is_expected.to validate_uniqueness_of :email }
-  it { is_expected.to validate_presence_of :password_digest }
+  it { is_expected.to validate_presence_of :password }
   it { should have_secure_password }
 
+  it { should have_many(:created_stickies) }
+  it { should have_many(:created_boards) }
+  it { should have_many(:edited_stickies) }
+  it { should have_many(:edited_boards) }
+  it { should have_many(:collaborations) }
   it { should have_many(:boards).through(:collaborations) }
-  it { should have_many(:stickies) }
 
 end
