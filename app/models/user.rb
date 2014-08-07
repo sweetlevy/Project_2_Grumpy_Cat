aed95f64ac5e1d9a2e1e532b9128e2e4246da1a0
sweetlevy <saritlevy@hotmail.com>
2014-08-07 11:25:16 -0400

    rspec test code complete for models. deleted the rspec test code for the app's welcome page because welcome page is no longer being used.

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   has_many :boards, through: :collaborations
-  validates :username, :name, :email, :password_digest, presence: true
+  validates :username, :name, :email, :password, presence: true
   validates :username, :email, uniqueness: true
 
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -2,14 +2,18 @@ require 'rails_helper'
 
 describe Board do
 
-  it { should have_many(:stickies).through(:categories) }
+  it { should have_many(:stickies).through(:categories).dependent(:destroy) }
   it { should have_many(:collaborations) }
-  it { should belong_to(:creator) }
-  it { should belong_to(:editor) }
+  it { should belong_to(:creator).class_name('User').with_foreign_key('creator_id') }
+  it { should belong_to(:editor).class_name('User').with_foreign_key('editor_id') }
   it { should have_many(:users).through(:collaborations) }
   it { should have_many(:categories) }
 
-  it { is_expected.to validate_presence_of :title }
-  it { is_expected.to validate_uniqueness_of :title }
+# validates :title on :create
+  describe "validate presence and uniqueness of title on create" do
+      subject { Board.new }
+      it { should validate_presence_of(:title) }
+      it { should validate_uniqueness_of(:title) }
+  end
 
 end
