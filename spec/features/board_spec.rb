bbbf5f314fdd18d0e2b938736820541fabc210c3
sweetlevy <saritlevy@hotmail.com>
2014-08-07 20:45:57 -0400

    tests for user sign-in feature and create board feature and added 'new_board' id to app/views/users/profile.html.erb

--- spec/features/board_spec.rb
+++ spec/features/board_spec.rb
@@ -1,33 +1,35 @@
 require 'rails_helper'
 
-describe 'board' do
-  let(:charlye) { User.new(
+describe 'board', js: true do
+  let(:charlye) { User.create(
     username: 'Charlye',
     name: 'Charlye Tran',
     password: "123",
     password_confirmation: "123",
     email: "charlyetran@gmail.com")}
 
-  let(:board3) { Board.new(
+  let(:board3) { Board.create(
     title: 'Three',
     creator_id: charlye.id)}
 
-  let(:collab1) { Collaboration.new(
+  let(:collab1) { Collaboration.create(
     user_id: sarit.id,
     board_id: board1.id)}
 
-  let(:category1) { Category.new(
+  let(:category1) { Category.create(
     title: 'Pain Points',
     board_id: board1.id)}
 
-  let(:sticky1) { Sticky.new(
+  let(:sticky1) { Sticky.create(
     title: 'Age',
     content: '28',
     category_id: category1.id)}
 
   it "should show the stickies for a particular board" do
-    visit(user_path(@charlye))
-    click_on "Three"
-    expect(page).to have_content("28")
+    visit(user_path(:charlye))
+    click_link('new_board')
+    fill_in 'board_title', with: 'Five'
+    click_button 'Create Board'
+
   end
 end
